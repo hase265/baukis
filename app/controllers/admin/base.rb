996b3a624d3bc7faae5ab86cdadf974ce077596c
MasayaHasegawa <mhase0114@gmail.com>
2017-11-29 13:36:22 +0900

    管理者ページの強制ログアウト

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,7 +1,7 @@
 class Admin::Base < ApplicationController
   before_action :authorize
   before_action :check_account
-
+  before_action :check_timeout
 
   private
   def current_administrator
@@ -21,9 +21,24 @@ class Admin::Base < ApplicationController
   end
 
   def check_account
-    if current_administrator && !current_staff_member.active?
+    if current_administrator && !current_administrator.active?
       session.delete(:administrator_id)
       flash.alert = 'アカウントが無効になりました'
       redirect_to :admin_root
+    end
+  end
+
+  TIMEOUT = 60.minutes
+
+  def check_timeout
+    if current_administrator
+      if session[:last_access_time] >= TIMEOUT.ago
+        session[:last_access_time] = Time.current
+      else
+        session.delete(:administrator_id)
+        flash.alert = 'セッションがタイムアウトしました'
+        redirect_to :admin_login
+      end
+    end
   end
 end
